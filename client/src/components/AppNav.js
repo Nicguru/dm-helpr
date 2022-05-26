f4fcd8a55ac19b9b4d873b4cfc12e0fa3f03f256
Nicholas <officialnicguru@gmail.com>
2022-05-26 01:00:47 -0700

    styled navbar

--- client/src/components/AppNav.js
+++ client/src/components/AppNav.js
@@ -1,52 +1,78 @@
+import {
+  faDice,
+  faDiceD20,
+  faDungeon,
+  faStore,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  Navbar,
+  Container,
+  Nav,
+  NavDropdown,
+  Stack,
+  Button,
+} from "react-bootstrap";
 const AppNav = () => {
   return (
     <header>
-      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            Fixed navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarCollapse">
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Link
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand as={Nav.Link}>
+            <Stack direction="horizontal" gap={2}>
+              <FontAwesomeIcon icon={faDiceD20} size="2x" />
+              <span className="cinzel unselectable d-none d-md-block">
+                dmHelpr
+              </span>
+            </Stack>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse>
+            <Nav className="me-auto mt-3 mt-lg-0" variant="pills" justify>
+              <Nav.Item>
+                <Nav.Link active>
+                  <FontAwesomeIcon
+                    icon={faDungeon}
+                    size="xl"
+                    className="d-none d-lg-block mx-auto"
+                  />
+                  Dashboard
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <FontAwesomeIcon
+                    icon={faStore}
+                    size="xl"
+                    className="d-none d-lg-block mx-auto"
+                  />
+                  Community
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link>
+                  <FontAwesomeIcon
+                    icon={faDice}
+                    size="xl"
+                    className="d-none d-lg-block mx-auto"
+                  />
+                  Generators
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+            <Nav className="me-2 mt-3 mt-lg-0">
+              <Stack direction="horizontal" gap={2}>
+                <Button variant="primary" className="col-6">
+                  Sign In
+                </Button>
+                <Button variant="secondary" className="col-6">
+                  Register
+                </Button>
+              </Stack>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
